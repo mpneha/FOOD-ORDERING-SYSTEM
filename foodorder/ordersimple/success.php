b86fc822202175285b892433c325d4dd449d372c
Neha M P <75907901+mpneha@users.noreply.github.com>
2021-12-09 20:00:25 +0530

    Add files via upload

---
+++ foodorder/ordersimple/success.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+<head>
+<body bgcolor="pink">
+<h1 style="color:purple;">Thankyou for ordering</h1>
+<h2 style="color:blue;"> Your order is successfull.</h2>
+<h2 style="color:blue;">Kindly confirm with the admin and pay the amount.</h2>
+<h2 style="color:red;">Click below inorder to go back to the menu page.</h2>
+<a href="index.php">Click here</a>
+<h2 style="color:red">Click below inorder to go back to signup page.</h2>
+<a href="register.php">Click here</a>
+<h2 style="color:red">Click below inorder to know the purchase details.</h2>
+<a href="summary.php">Click here</a>
+ </body>
+</head>
+<html>
